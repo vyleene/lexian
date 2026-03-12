b46eb6a62c3659262caf21cc941b99a996c34d4c
vyleene <lukeruaya700@gmail.com>
2026-03-12 18:15:38 +0800

    Added base app view model

---
+++ src/models/AppModel.ts
@@ -0,0 +1,19 @@
+export type PanelKey = 'students' | 'programs' | 'colleges'
+export type ThemeMode = 'dark' | 'light'
+
+export const DEFAULT_PANEL: PanelKey = 'students'
+export const DEFAULT_THEME: ThemeMode = 'dark'
+
+export interface AppViewModel {
+  activePanel: PanelKey
+  theme: ThemeMode
+  showSplash: boolean
+  hideSplash: boolean
+}
+
+export interface AppViewActions {
+  onToggleTheme: () => void
+  onSelectPanel: (panel: PanelKey) => void
+}
+
+export type AppViewProps = AppViewModel & AppViewActions
